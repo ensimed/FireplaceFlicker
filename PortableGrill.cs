0a93dc03ea4f867a94aa75124a1fd2619b4d9532
ensimed <69088479+ensimed@users.noreply.github.com>
2023-08-07 23:15:41 +0200

    script for portable grill

---
+++ PortableGrill.cs
@@ -0,0 +1,39 @@
+using MSCLoader;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace FireplaceFlicker
+{
+    public class PortableGrill : MonoBehaviour
+    {
+        private Light FireplaceLight;
+        private bool Flicker = false;
+        private float Delay;
+        private GameObject LightObject;
+
+        void Update()
+        {
+            LightObject = GameObject.Find("ITEMS/grill(itemx)/Fireplace/Flame/FireEffects/Light");
+            FireplaceLight = GameObject.Find("ITEMS/grill(itemx)/Fireplace/Flame/FireEffects/Light").GetComponent<Light>();
+            FireplaceLight.shadows = LightShadows.Hard;
+            if (Flicker == false && LightObject.activeSelf == true)
+            {
+                StartCoroutine(FlickerControl());
+            }
+            else
+            {
+                StopCoroutine(FlickerControl());
+            }
+        }
+
+        IEnumerator FlickerControl()
+        {
+            Flicker = true;
+            FireplaceLight.intensity = Random.Range(0.5f, 1.5f);
+            Delay = Random.Range(0.1f, 0.3f);
+            yield return new WaitForSeconds(Delay);
+            Flicker = false;
+        }
+    }
+}
